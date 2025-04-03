48fd2cbbe70bc28f520d5565ef671d6db9ee136f
oskwame <nanaqwamee124@gmail.com>
2025-04-03 00:11:19 -1200

    Update with SEO

--- app/robot.txt/robot.ts
+++ app/robot.txt/robot.ts
@@ -1,8 +1,8 @@
 export async function GET() {
     const robots = `User-agent: *
   Allow: /
-  Sitemap: https://oskwame.vercel.app/sitemap.xml`;
-  
+  Sitemap: https://oskwame.vercel.app/sitemap.xml;
+  `;
     return new Response(robots, {
       headers: {
         "Content-Type": "text/plain",
