35db4b97b92b7c981a28f053fe9771244309a12c
oskwame <nanaqwamee124@gmail.com>
2025-04-02 23:38:30 -1200

    Update with SEO

--- app/sitemap.xml/Route.ts
+++ app/sitemap.xml/Route.ts
@@ -1,20 +1,15 @@
-export async function GET() {
-    const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://oskwame.vercel.app";
-  
-    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        <url>
-          <loc>${baseUrl}</loc>
-          <changefreq>monthly</changefreq>
-          <priority>1.0</priority>
-        </url>
-      </urlset>`;
-  
-    return new Response(sitemap, {
-      headers: {
-        "Content-Type": "application/xml",
-      },
-    });
-  }
-  
-  
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://oskwame.vercel.app/</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <priority>1.0</priority>
+    </url>
+  </urlset>`;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.status(200).send(sitemap);
+}
